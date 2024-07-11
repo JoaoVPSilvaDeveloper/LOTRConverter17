80644eddbfacd3f133de90b1251bb045994599ac
JoaoVPSilvaDeveloper <joaovpsilvadeveloper@gmail.com>
2024-07-12 00:01:32 +0100

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Joao Silva on 10/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
